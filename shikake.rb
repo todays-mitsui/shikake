196ede46e3cca0796df55c57c80b3a9370199468
三井 翔吾 <mitsui@queserser-j.co.jp>
2014-08-12 16:04:52 +0900

    EventTrackタグ, CallCVタグも拾うように変更。ログの保存の仕方を少しだけマシにした。
    TODO
    ・キャッシュをメモリからファイルへ
    ・logファイルにタイムスタンプを打つ
    ・spだけのクローニング
    ・UserAgentをちゃんとする

--- shikake.rb
+++ shikake.rb
@@ -7,12 +7,15 @@ class Shikake < Anemone::Core
 		super
 		@opts = {
 			:skip_query_strings => true,
-			:skip_link_patterns => [/\.jpe?g$/i, /\.gif$/i, /.png$/i, /\.pdf$/i]
+			:skip_link_patterns => [/\.jpe?g$/i, /\.gif$/i, /.png$/i, /\.pdf$/i, %r|/upimg/|],
+			:delay => 0.5,
+			:verbose => true
 		}
 	end
 
 	def scan
-		on_every_page &scan_atag
+		on_pages_like /(\.html?|\.php|\/)$/, &scan_tag
+		#on_every_page &scan_atag
 		@start_time = Time.now.to_i
 		run
 		@end_time = Time.now.to_i
@@ -20,102 +23,187 @@ class Shikake < Anemone::Core
 		@scan_result
 	end
 
-	def scan_atag
+	def scan_tag
 		@scan_result = Atags.new
 		lambda do |page|
-			@scan_result.push(page.url, page.atags) if page.doc
-			#if page.doc
-			#	a = page.atags
-			#	puts page.url
-			#	p a
-			#	@scan_result.push(page.url, a)
-			#end
+			@scan_result.push(page.url, page.doc.title, page.tags) if page.doc
 		end
 	end
 
 	class Anemone::Page
-		@atags = false
+		@tags = false
 
-		def atags
-			find_atag unless @atags
-			@atags
+		def tags
+			find_tags unless @tags
+			@tags
 		end
 
-		def find_atag
-			r_ga = /(\[.*_setAccount.+(UA-\d+-\d+).+\])/
-			r_univ = /(ga\s*\(\s*('create'|"create").*(UA-\d+-\d+).+;)/
-			@atags = []
+		def find_tags
+			r_ga_old = /(\[.*_setAccount.+(UA-\d+-\d+).+\])/i
+			r_ga_new = /(ga\s*\(\s*('create'|"create").*(UA-\d+-\d+).+;)/i
+			r_event_old = /_?gaq.push\s*\(\s*(\[('_trackEvent'|"_trackEvent").*\])\s*\)/i
+			r_event_new = /ga\s*\(('send'|"send").*,.*('event'|"event").*\)/i
+			r_callcv_old = /_?gaq.push\s*\(\s*(\[('_trackPageview'|"_trackPageview").*\])\s*\)/i
+			r_callcv_new = /ga\s*(\(('send'|"send").*,.*('pageview'|"pageview").*\))/i
+			@tags = Hash.new {|hash, key| hash[key] = []}
 			@doc.css("script").each do |el|
-				@atags << {kind: :univ, id: $3} if r_univ.match(el.text)
-				@atags << {kind: :ga, id: $2} if r_ga.match(el.text)
+				@tags[:ga] << {id: $2} if r_ga_old.match(el.text)
+				@tags[:univ] << {id: $3} if r_ga_new.match(el.text)
+			end
+			@doc.css("a").each do |el|
+				@tags[:event_old] << {tag: $1, val: el.inner_text} if r_event_old.match(el.attribute("onclick"))
+				@tags[:event_new] << {tag: $1, val: el.inner_text} if r_event_new.match(el.attribute("onclick"))
+				@tags[:callcv_old] << {tag: $1, val: el.inner_text} if r_callcv_old.match(el.attribute("onclick"))
+				@tags[:callcv_new] << {tag: $1, val: el.inner_text} if r_callcv_new.match(el.attribute("onclick"))
 			end
 		end
 	end
 end
 
 class Atags
+	@@kinds = [:ga, :univ, :event_old, :event_new, :callcv_old, :callcv_new]
+
 	def initialize
-		@tag_pages = {}
-		@no_tag_pages = []
+		@tags = Hash.new {|hash, key| hash[key] = {}}
 	end
 
 	def pages
-		@tag_pages.keys + @no_tag_pages
-	end
-	
-	def tag_pages
-		@tag_pages.keys
+		@tags.keys
 	end
 
-	def no_tag_pages
-		@no_tag_pages
-	end
-
-	def tags
-		@tag_pages.values.flatten.uniq
-	end
-
-	def tag_ids
-		tags.map{|tag| tag[:id]}.uniq
-	end
-	
-	def push(url, tags)
-		if tags && !tags.empty?
-			stock_tag(url, tags)
-		else
-			stock_no_tag(url)
-		end
-	end
-
-	private
-
-		def stock_tag(url, tags)
-			if @tag_pages.include?(url)
-				@tag_pages[url] += tags
-			else
-				@tag_pages[url] = tags
-			end
+	@@kinds.each do |kind|
+		define_method "#{kind}_pages" do
+			@tags.select{|key, val| val[kind] && !val[kind].empty?}.keys
 		end
 
-		def stock_no_tag(url)
-			@no_tag_pages << url unless @tag_pages.include?(url) || @no_tag_pages.include?(url)
+		define_method "no_#{kind}_pages" do
+			@tags.select{|key, val| !val[kind] || val[kind].empty?}.keys
 		end
+
+		#define_method "#{kind}_tags" do
+		#	@tags.map{|key, val| val[kind] unless !val[kind] || val[kind].empty?}.compact.uniq
+		#end
+	end
+
+	def ga_tags
+		@tags.map{|key, val| val[:ga].map{|tag| tag[:id]} if val[:ga]}.flatten.compact.uniq
+	end
+	def univ_tags
+		@tags.map{|key, val| val[:univ].map{|tag| tag[:id]} if val[:univ]}.flatten.compact.uniq
+	end
+
+	def event_old_tags
+		@tags.map{|key, val| val[:event_old].map{|tag| tag[:tag]} if val[:event_old]}.flatten.compact.uniq
+	end
+	def event_new_tags
+		@tags.map{|key, val| val[:event_new].map{|tag| tag[:tag]} if val[:event_new]}.flatten.compact.uniq
+	end
+
+	def callcv_old_tags
+		@tags.map{|key, val| val[:callcv_old].map{|tag| tag[:tag]} if val[:callcv_old]}.flatten.compact.uniq
+	end
+	def callcv_new_tags
+		@tags.map{|key, val| val[:callcv_new].map{|tag| tag[:tag]} if val[:callcv_new]}.flatten.compact.uniq
+	end
+
+	def ga_tags_url
+		@tags.map{|url, val| url if val[:ga]}.compact
+	end
+	def no_univ_tags_url
+		@tags.map{|url, val| url if !val[:univ]}.compact
+	end
+	def event_old_tags_url
+		@tags.map{|url, val| "url: #{url}\ntag: #{val[:event_old].map{|tag| tag[:tag]}}\n" if val[:event_old]}.compact
+	end
+	def event_new_tags_url
+		@tags.map{|url, val| "url: #{url}\ntag: #{val[:event_new].map{|tag| tag[:tag]}}\n" if val[:event_new]}.compact
+	end
+	def callcv_old_tags_url
+		@tags.map{|url, val| "url: #{url}\ntag: #{val[:callcv_old].map{|tag| tag[:tag]}}\n" if val[:callcv_old]}.compact
+	end
+	def callcv_new_tags_url
+		@tags.map{|url, val| "url: #{url}\ntag: #{val[:callcv_new].map{|tag| tag[:tag]}}\n" if val[:callcv_new]}.compact
+	end
+
+	def push(url, title, tags)
+		tags.each do |kind, val|
+			@tags[url][kind] = [] unless @tags[url][kind]
+			@tags[url][kind] += val
+			@tags[url][:title] = title
+		end
+	end
 end
 
-if ARGV[0]
-	shikake = Shikake.new(ARGV[0])
+root_url = ARGV[0]
+
+if root_url
+	filename = root_url.gsub(/[\\\/:*?"<>|]/, "").gsub(/^https?/, "")
+	filepath = "log/#{filename}.txt"
+
+	shikake = Shikake.new(root_url)
 	scan_result = shikake.scan
 	result = <<-EOD
-url:          #{ARGV[0]}
-all pages:    #{scan_result.pages.length} pages
-tag pages:    #{scan_result.tag_pages.length} pages
-no tag pages: #{scan_result.no_tag_pages.length} pages
-tags:         #{scan_result.tags}
+url:                #{root_url}
+all pages:          #{scan_result.pages.length} pages
+
+ga tags:            #{scan_result.ga_pages.length} pages
+no ga tags:         #{scan_result.no_ga_pages.length} pages
+tag ids:            #{scan_result.ga_tags}        
+
+univ tags:          #{scan_result.univ_pages.length} pages
+no univ tags:       #{scan_result.no_univ_pages.length} pages
+tag ids:            #{scan_result.univ_tags}
+
+old event tags:     #{scan_result.event_old_pages.length} pages
+no old event tags:  #{scan_result.no_event_old_pages.length} pages
+tag ids:            #{scan_result.event_old_tags}        
+
+new event tags:     #{scan_result.event_new_pages.length} pages
+no new event tags:  #{scan_result.no_event_new_pages.length} pages
+tag ids:            #{scan_result.event_new_tags}        
+
+old callcv tags:    #{scan_result.callcv_old_pages.length} pages
+no old callcv tags: #{scan_result.no_callcv_old_pages.length} pages
+tag ids:            #{scan_result.callcv_old_tags}        
+
+new callcv tags:    #{scan_result.callcv_new_pages.length} pages
+no new callcv tags: #{scan_result.no_callcv_new_pages.length} pages
+tag ids:            #{scan_result.callcv_new_tags}        
 	EOD
+	puts ""
 	puts result
-	File.open("log/scan_result.txt", "w+") do |file|
+	FileUtils.mkdir_p("log/") unless FileTest.exist?("log/")
+	File.open(filepath, "w+") do |file|
 		file.write result
 		file.write "\n"
-		file.write scan_result.no_tag_pages.join("\n")
+		file.write "以下のページに旧GoogleAnalyticsタグが見つかりました。\n\n"
+		file.write scan_result.ga_tags_url.join("\n") + "\n"
+		file.write "=================================================================\n"
+		file.write "\n"
+		file.write "以下のページにはユニバーサルAnalyticsタグが見つかりませんでした。\n\n"
+		file.write scan_result.no_univ_tags_url.join("\n") + "\n"
+		file.write "=================================================================\n"
+		file.write "\n"
+		file.write "以下の旧イベントタグが見つかりました。\n\n"
+		file.write scan_result.event_old_tags_url.join("\n")
+		file.write "=================================================================\n"
+		file.write "\n"
+		file.write "以下の新イベントタグが見つかりました。\n\n"
+		file.write scan_result.event_new_tags_url.join("\n")
+		file.write "=================================================================\n"
+		file.write "\n"
+		file.write "以下の旧Call CVタグが見つかりました。\n\n"
+		file.write scan_result.callcv_old_tags_url.join("\n")
+		file.write "=================================================================\n"
+		file.write "\n"
+		file.write "以下の新Call CVタグが見つかりました。\n\n"
+		file.write scan_result.callcv_new_tags_url.join("\n")
+		file.write "=================================================================\n"
 	end
+	puts "\n#{File.expand_path(filepath)}"
+	puts "に結果を保存しました。"
+else
+	puts "サイトのURLを指定して実行してください。"
+	puts "例:"
+	puts "ruby #{$0} http://www.example.com/"
 end
