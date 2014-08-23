430dd184aba639a1807a5042c2f91bf59835e7fc
Mitsui <todays.mitsui@gmail.com>
2014-08-23 16:26:42 +0900

    GoogleAdWords, Yahoo!スポンサードサーチ, Yahoo!ディスプレイ広告(YDN)のタグをスキャン対象に追加。 Click taleについては考え中...。

--- shikake.rb
+++ shikake.rb
@@ -6,17 +6,23 @@ require './lib/profile_print'
 require './lib/spider'
 
 REGEXP = {
-	:ga        => /(\[.*_setAccount.+(UA-\d+-\d+).+\])/i,
-	:univ      => /(ga\s*\(\s*('create'|"create").*(UA-\d+-\d+).+;)/im,
-	:disp_feat => /ga\s*\(\s*('require'|"require")\s*,\s*('displayfeatures'|"displayfeatures")\s*\)/i,
-	:old_event => /_?gaq.push\s*\(\s*(\[('_trackEvent'|"_trackEvent").*\])\s*\)/i,
-	:new_event => /(ga\s*\(('send'|"send").*,.*('event'|"event").*\))/i,
-	:old_pv    => /_?gaq.push\s*\(\s*(\[('_trackPageview'|"_trackPageview").*\])\s*\)/i,
-	:new_pv    => /ga\s*(\(('send'|"send").*,.*('pageview'|"pageview").*\))/i,
-	:ytm       => %r{"//s\.yjtag\.jp/tag\.js#(site=[^"]+)"},
+	:ga           => /(\[.*_setAccount.+(UA-\d+-\d+).+\])/i,
+	:univ         => /(ga\s*\(\s*('create'|"create").*(UA-\d+-\d+).+;)/im,
+	:disp_feat    => /ga\s*\(\s*('require'|"require")\s*,\s*('displayfeatures'|"displayfeatures")\s*\)/i,
+	:old_event    => /_?gaq.push\s*\(\s*(\[('_trackEvent'|"_trackEvent").*\])\s*\)/i,
+	:new_event    => /(ga\s*\(('send'|"send").*,.*('event'|"event").*\))/i,
+	:old_pv       => /_?gaq.push\s*\(\s*(\[('_trackPageview'|"_trackPageview").*\])\s*\)/i,
+	:new_pv       => /ga\s*(\(('send'|"send").*,.*('pageview'|"pageview").*\))/i,
+	:g_adwords_cv => /google_conversion_id\s*=\s*(\d+).+google_conversion_label\s*=\s*["'](\w*)["'].+google_remarketing_only\s*=\s*false/im,
+	:g_remarke    => /google_conversion_id\s*=\s*(\d+).+google_conversion_label\s*=\s*["'](\w*)["'].+google_remarketing_only\s*=\s*true/im,
+	:yss_cv       => /yahoo_conversion_id\s*=\s*(\d+).+yahoo_conversion_label\s*=\s*["'](\w*)["']/im,
+	:ydn_remarke  => /yahoo_retargeting_id\s*=\s*["'](\w+)["'].+yahoo_retargeting_label\s*=\s*["'](\w*)["']/im,
+	:ydn_cv       => %r{sccount_id\s*=\s*["']([\w\.]+)["'].+//b90\.yahoo\.co\.jp/c\?}im,
+	:ytm          => %r{"//s\.yjtag\.jp/tag\.js#(site=[^"]+)"},
 }
 
 spider = Shikake::Spider.new(ARGV[0])
+
 spider.train(:ga ,{
 	:name => "GoogleAnalytics",
 	:selector => "script",
@@ -59,6 +65,41 @@ spider.train(:new_pv ,{
 	:regexp => REGEXP[:new_pv],
 	:val => lambda{|el,md| "id: #{md[1]}, href: \"#{el.attribute("href")}\""}
 })
+spider.train(:g_adwords_cv ,{
+	:name => "GoogleAdWordsConversion",
+	:selector => "script",
+	:before => lambda{|el| el.text},
+	:regexp => REGEXP[:g_adwords_cv],
+	:val => lambda{|el,md| "id: #{md[1]}, label: #{md[2].empty? ? '無し' : md[2]}"}
+})
+spider.train(:g_remarke ,{
+	:name => "GoogleAdWordsRemarketing",
+	:selector => "script",
+	:before => lambda{|el| el.text},
+	:regexp => REGEXP[:g_remarke],
+	:val => lambda{|el,md| "id: #{md[1]}, label: #{md[2].empty? ? '無し' : md[2]}"}
+})
+spider.train(:yss_cv ,{
+	:name => "YSS_Conversion",
+	:selector => "script",
+	:before => lambda{|el| el.text},
+	:regexp => REGEXP[:yss_cv],
+	:val => lambda{|el,md| "id: #{md[1]}, label: #{md[2].empty? ? '無し' : md[2]}"}
+})
+spider.train(:ydn_remarke ,{
+	:name => "YDN_Remarketing",
+	:selector => "script",
+	:before => lambda{|el| el.text},
+	:regexp => REGEXP[:ydn_remarke],
+	:val => lambda{|el,md| "id: #{md[1]}, label: #{md[2].empty? ? '無し' : md[2]}"}
+})
+spider.train(:ydn_cv ,{
+	:name => "YDN_Conversion",
+	:selector => "script",
+	:before => lambda{|el| el.text},
+	:regexp => REGEXP[:ydn_cv],
+	:val => lambda{|el,md| "id: #{md[1]}"}
+})
 spider.train(:ytm ,{
 	:name => "Yahoo!TagManager",
 	:selector => "script",
@@ -66,6 +107,7 @@ spider.train(:ytm ,{
 	:regexp => REGEXP[:ytm],
 	:val => lambda{|el,md| "id: #{md[1]}"}
 })
+
 spider.scan.show
 
 
